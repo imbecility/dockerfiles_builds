3455f69c48eb708086a0079ddc64b9dea7d61a34
imbecility <223640953+imbecility@users.noreply.github.com>
2026-08-18 16:00:42 +0300

    refactoring webgl rollback 3

--- Camoufox/probe.py
+++ Camoufox/probe.py
@@ -18,6 +18,14 @@ def main() -> None:
         context = browser.new_context()
         context.set_default_timeout(15000)
         run_firefox_smoke_suite(context, extended=False)
+        try:
+            context.set_default_timeout(10000)
+            page = context.new_page()
+            page.goto("https://ya.ru", wait_until="domcontentloaded")
+            print(f'заголовок страницы: "{page.title()}"')
+            page.close()
+        except:
+            pass
         context.close()
         browser.close()
 
