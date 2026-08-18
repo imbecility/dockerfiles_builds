655e9d9ec964ce18a08e8cbf5dbc9a84a6e615ed
imbecility <223640953+imbecility@users.noreply.github.com>
2026-08-18 10:21:26 +0300

    refactoring webgl

--- Camoufox/server.py
+++ Camoufox/server.py
@@ -21,7 +21,12 @@ if __name__ == "__main__":
         "i_know_what_im_doing": True,
         "config": {
             "forceScopeAccess": True
-        }
+        },
+        "firefox_user_prefs": {
+            "webgl.force-enabled": True,
+            "webgl.disabled": False,
+            "layers.acceleration.force-enabled": True,
+        },
     }
 
     proxy_url = os.getenv("PROXY_SERVER")
